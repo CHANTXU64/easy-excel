2b7f9f110e7b351b03c33e6c19f85951797b9bb8
CHANTXU64 <chantxu@outlook.com>
2021-04-10 22:03:58 +0800

    pkg update

---
+++ excel.js
@@ -0,0 +1,7 @@
+/**
+ * Copyright (c) ChantXu64
+ * LICENCE: MIT - please refer to LICENSE file included with this module
+ * or https://github.com/CHANTXU64/easy-excel/blob/main/LICENSE
+ */
+
+module.exports = require('./src/file.ts');
